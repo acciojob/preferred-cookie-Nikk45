7c4a11c5e705c8699d118ce4be00ad81962dba69
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-28 15:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-// //your JS code here. If required.
-// let form = document.getElementById('form');
+//your JS code here. If required.
+let form = document.getElementById('form');
 
-// let size = document.getElementById('fontsize');
-// let color = document.getElementById('fontcolor');
+let size = document.getElementById('fontsize');
+let color = document.getElementById('fontcolor');
 
-// form.addEventListener('submit',(e)=>{
-// 	e.preventDefault();
-// 	let fontSize = size.value;
-// 	let fontColor = color.value;
-// 	console.log(fontColor,fontSize);
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	let fontSize = size.value;
+	let fontColor = color.value;
+	console.log(fontColor,fontSize);
 
-// 	document.cookie = `fontsize = ${fontSize}; path=https://course.acciojob.com/open-web-editor?question=c594255e-e53e-49ef-b3cb-0e68e8866e36`;
-// 	document.cookie = `fontcolor = ${fontColor}; path=https://course.acciojob.com/open-web-editor?question=c594255e-e53e-49ef-b3cb-0e68e8866e36`;
+	document.cookie = `fontsize = ${fontSize}; path=https://course.acciojob.com/open-web-editor?question=c594255e-e53e-49ef-b3cb-0e68e8866e36`;
+	document.cookie = `fontcolor = ${fontColor}; path=https://course.acciojob.com/open-web-editor?question=c594255e-e53e-49ef-b3cb-0e68e8866e36`;
 
-// 	document.body.style.fontSize = fontSize+"px";
-// 	document.body.style.color = fontColor;
-// })
+	document.body.style.fontSize = fontSize+"px";
+	document.body.style.color = fontColor;
+})
 
-// document.addEventListener('DOMContentLoaded', (e)=>{
-// 	// e.preventDefault();
-// 	let styles = document.cookie.split(';');
-// 	// console.log(styles);
-// 	let arr = [];
-// 	styles.forEach((style)=>{
-// 		arr.push(style.split("="));	
-// 	})
+document.addEventListener('DOMContentLoaded', (e)=>{
+	// e.preventDefault();
+	let styles = document.cookie.split(';');
+	// console.log(styles);
+	let arr = [];
+	styles.forEach((style)=>{
+		arr.push(style.split("="));	
+	})
 
-// 		// console.log(arr);
+		// console.log(arr);
 	
-// 	let newColor, newSize;
+	let newColor, newSize;
 
-// 	for(let ar of arr){
-// 		// console.log(ar);
-// 		if(ar[0] === " fontsize"){
-// 			newSize = ar[1];
-// 			// console.log(ar[1]);
-// 		}
-// 		if(ar[0] === " fontcolor"){
-// 			newColor = ar[1];
-// 			// console.log(ar[1]);
-// 		}
-// 	}
+	for(let ar of arr){
+		// console.log(ar);
+		if(ar[0] === " fontsize"){
+			newSize = ar[1];
+			// console.log(ar[1]);
+		}
+		if(ar[0] === " fontcolor"){
+			newColor = ar[1];
+			// console.log(ar[1]);
+		}
+	}
 	
-// 	size.value = newSize;
-// 	color.value = newColor;
-// 	document.body.style.fontSize = newSize+"px";
-// 	document.body.style.color = newColor;
-// });
+	size.value = newSize;
+	color.value = newColor;
+	document.body.style.fontSize = newSize+"px";
+	document.body.style.color = newColor;
+});
 
 
 
